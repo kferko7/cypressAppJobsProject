3a4b596e8feade998e539755c1589408fe49f252
kferko7 <kim.ferko@hotmail.com>
2022-06-27 11:15:28 +0200

    Update master on Api calls

--- cypress/integration/pages/API/jobOffersUrlMiamiFulltime.js
+++ cypress/integration/pages/API/jobOffersUrlMiamiFulltime.js
@@ -5,14 +5,13 @@ describe('get api user tests', () => {
 	it('get payload request url web-api/en/job', () => {
 		cy.request({
 			method: 'GET',
-			url: 'https://www.appjobs.com/miami'
+			url: 'https://www.appjobs.com/miami?type=full_time'
 			
 		}).then((res) => {
 			expect(res.status).to.eq(200)
-			expect(res.body.meta.pagination.limit).to.eq(14)
+			expect(res.body.meta.pagination.limit).to.eq(20)
 			expect(res.body.meta.city).to.eq("Miami")
 			expect(res.body.meta.job_offers).to.eq(3)
-
 						
 		})
 	})
